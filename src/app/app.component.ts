c10f7df6c77b164ab32fde281c2b63ed4e285920
César González <cedgo1997@gmail.com>
2024-07-09 04:29:33 -0400

    ADD: Cool that we have routes now, huh?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { DynamicFormComponent } from './components/dynamic-form/dynamic-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DynamicFormComponent],
+  imports: [RouterOutlet, DynamicFormComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { RegisterUserComponent } from './components/register-user/register-user.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
-export const routes: Routes = [];
+export const ROUTES: Routes = [
+    { path: 'register-user', component: RegisterUserComponent },
+    { path: '', component: WelcomeComponent },
+    { path: '**', redirectTo: '' }
+]
