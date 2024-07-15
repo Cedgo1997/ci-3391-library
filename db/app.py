39d29b5a68729b352b05a388d03bd3d994af98fa
freddcastro <eduardofredd@gmail.com>
2024-07-14 21:41:00 -0400

    endpoint para registrar libro

--- db/app.py
+++ db/app.py
@@ -7,13 +7,16 @@ app = Flask(__name__)
 # Conexion a la base de datos
 connect_to_db()
 
+
 @app.route("/")
 def root():
     return "Hello World!"
 
+
 @app.route("/usuarios")
 def usuarios():
     return "usuarios"
+
 
 @app.route("/usuarios/<string:cedula>")
 def usuario(cedula):
@@ -25,6 +28,7 @@ def usuario(cedula):
     connection.close()
     return jsonify(user)
 
+
 @app.route("/usuarios_crear", methods=["POST"])
 def crear_usuario():
     connection = connect_to_db()
@@ -32,15 +36,36 @@ def crear_usuario():
 
     # Obtener los datos del usuario desde el body del request
     data = request.json
-    
+
     # llamada al procedimiento almacenado usando CALL
-    cursor.execute(f"CALL crear_usuario('{data['in_cedula']}', '{data['in_nombre']}', '{data['in_apellido']}', '{data['in_fecha_nacimiento']}', '{data['in_correo']}', '{data['in_tipo_usuario']}', null)")
-        
+    cursor.execute(
+        f"CALL crear_usuario('{data['in_cedula']}', '{data['in_nombre']}', '{data['in_apellido']}', '{data['in_fecha_nacimiento']}', '{data['in_correo']}', '{data['in_tipo_usuario']}', null)")
+
     cursor.close()
     connection.commit()
     connection.close()
 
     return jsonify({"message": "Usuario creado exitosamente"})
 
+
+@app.route("/registrar_nuevo_libro", methods=["POST"])
+def registrar_nuevo_libro():
+    connection = connect_to_db()
+    cursor = connection.cursor()
+
+    # Obtener los datos del usuario desde el body del request
+    data = request.json
+
+    cursor.execute(
+        f"CALL registrar_nuevo_libro('{data['in_isbn']}','{data['in_titulo']}', '{data['in_precio']}', '{data['in_edicion']}', '{data['in_fecha_publicacion']}', '{data['in_restriccion_edad']}', '{data['in_nombre_sucursal']}', '{data['in_nombre_editorial']}')"
+    )
+
+    cursor.close()
+    connection.commit()
+    connection.close()
+
+    return jsonify({"message": "Libro registrado exitosamente"})
+
+
 if __name__ == '__main__':
-    app.run(port=3000,debug=True)
+    app.run(port=3000, debug=True)
