8d640559a70d0f5e8faeaf9e0d826bb11f5ca702
EliezerMCR <eliezermanuelcr@gmail.com>
2024-07-18 05:47:38 -0400

    create tabs component and layout loans/returns and events views

--- src/app/views/events/events.component.ts
+++ src/app/views/events/events.component.ts
@@ -1,12 +1,81 @@
 import { Component } from '@angular/core';
+import { Field } from '../../interfaces/field.interface';
+import { DynamicFormComponent } from '../../components/dynamic-form/dynamic-form.component';
+import { TabsComponent } from '../../components/tabs/tabs.component';
 
 @Component({
   selector: 'app-events',
   standalone: true,
-  imports: [],
+  imports: [DynamicFormComponent, TabsComponent],
   templateUrl: './events.component.html',
-  styleUrl: './events.component.scss'
+  styleUrl: './events.component.scss',
 })
 export class EventsComponent {
+  isAssist: boolean = true; // Variable para alternar entre préstamo y devolución
 
+  // Obtener la fecha actual
+  currentDate: string = new Date().toISOString().split('T')[0];
+
+  // Obtener la fecha actual + 30 días
+  futureDate: string = new Date(Date.now() + 30 * 24 * 60 * 60 * 1000)
+    .toISOString()
+    .split('T')[0];
+
+  eventsFields: Field[] = [
+    {
+      label: 'Evento',
+      name: 'in_pk_evento',
+      type: 'select',
+      value: '',
+      required: true,
+    },
+    {
+      label: 'Correo asistente',
+      name: 'in_correo',
+      value: '',
+      type: 'email',
+      required: true,
+    },
+  ];
+
+  registerFields: Field[] = [
+    {
+      label: 'Fecha de inicio',
+      name: 'fecha_inicio',
+      type: 'date',
+      value: this.currentDate,
+      required: true,
+    },
+    {
+      label: 'Fecha final',
+      name: 'cedula_bibliotecario',
+      value: this.futureDate,
+      type: 'date',
+      required: true,
+    },
+    {
+      label: 'Sucursal',
+      name: 'nombre_sucursal',
+      value: '',
+      type: 'select',
+      required: true,
+    },
+  ];
+
+  fields: Field[] = this.eventsFields;
+
+  handleTabChange(index: number) {
+    this.isAssist = index === 0;
+    this.fields = this.isAssist ? this.eventsFields : this.registerFields;
+  }
+
+  handleFormSubmit(formData: any) {
+    if (this.isAssist) {
+      console.log('Registering Assistant:', formData);
+      // Lógica para registrar asistencia al evento
+    } else {
+      console.log('Create event:', formData);
+      // Lógica para crear un evento
+    }
+  }
 }
--- src/app/views/loan-return/loan-return.component.ts
+++ src/app/views/loan-return/loan-return.component.ts
@@ -1,12 +1,109 @@
 import { Component } from '@angular/core';
+import { Field } from '../../interfaces/field.interface';
+import { DynamicFormComponent } from '../../components/dynamic-form/dynamic-form.component';
+import { TabsComponent } from "../../components/tabs/tabs.component";
 
 @Component({
   selector: 'app-loan-return',
   standalone: true,
-  imports: [],
+  imports: [DynamicFormComponent, TabsComponent],
   templateUrl: './loan-return.component.html',
-  styleUrl: './loan-return.component.scss'
+  styleUrl: './loan-return.component.scss',
 })
 export class LoanReturnComponent {
+  isLoan: boolean = true; // Variable para alternar entre préstamo y devolución
 
+  // Obtener la fecha actual
+  currentDate: string = new Date().toISOString().split('T')[0];
+
+  // Obtener la fecha actual + 30 días
+  futureDate: string = new Date(Date.now() + 30 * 24 * 60 * 60 * 1000)
+    .toISOString()
+    .split('T')[0];
+
+  loanFields: Field[] = [
+    {
+      label: 'Serial Ejemplar',
+      name: 'serial_ejemplar',
+      type: 'text',
+      value: '',
+      required: true,
+    },
+    {
+      label: 'Cédula Bibliotecario',
+      name: 'cedula_bibliotecario',
+      value: '',
+      type: 'text',
+      required: true,
+    },
+    {
+      label: 'Cédula Lector',
+      name: 'cedula_lector',
+      type: 'text',
+      value: '',
+      required: true,
+    },
+    {
+      label: 'Fecha Inicio',
+      name: 'fecha_inicio',
+      type: 'date',
+      value: this.currentDate,
+      required: true,
+    },
+    {
+      label: 'Fecha Final',
+      name: 'fecha_final',
+      type: 'date',
+      value: this.futureDate,
+      required: true,
+    },
+  ];
+
+  returnFields: Field[] = [
+    {
+      label: 'Serial Ejemplar',
+      name: 'serial_ejemplar',
+      type: 'text',
+      value: '',
+      required: true,
+    },
+    {
+      label: 'Cédula Bibliotecario',
+      name: 'cedula_bibliotecario',
+      value: '',
+      type: 'text',
+      required: true,
+    },
+    {
+      label: 'Cédula Lector',
+      name: 'cedula_lector',
+      type: 'text',
+      value: '',
+      required: true,
+    },
+    {
+      label: 'Fecha Entrega',
+      name: 'fecha_entrega',
+      type: 'date',
+      value: this.currentDate,
+      required: true,
+    },
+  ];
+
+  fields: Field[] = this.loanFields;
+
+  handleTabChange(index: number) {
+    this.isLoan = index === 0;
+    this.fields = this.isLoan ? this.loanFields : this.returnFields;
+  }
+
+  handleFormSubmit(formData: any) {
+    if (this.isLoan) {
+      console.log('Registering loan:', formData);
+      // Lógica para registrar un préstamo
+    } else {
+      console.log('Registering return:', formData);
+      // Lógica para registrar una devolución
+    }
+  }
 }
