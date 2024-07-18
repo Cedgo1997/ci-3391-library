87172132e5c3c557a36831b5c3e724e436f2c23d
EliezerMCR <eliezermanuelcr@gmail.com>
2024-07-17 22:36:42 -0400

    add routes and ignore conexio to db

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { Routes } from '@angular/router';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { RegisterUserComponent } from './views/register-user/register-user.component';
+import { LoanReturnComponent } from './views/loan-return/loan-return.component';
+import { EventsComponent } from './views/events/events.component';
+import { SalesDonationsComponent } from './views/sales-donations/sales-donations.component';
+import { ReportsComponent } from './views/reports/reports.component';
 
 export const ROUTES: Routes = [
+    { path: 'events', component: EventsComponent },
+    { path: 'loan-return', component: LoanReturnComponent },
     { path: 'register-user', component: RegisterUserComponent },
+    { path: 'reports', component: ReportsComponent },
+    { path: 'sales-donations', component: SalesDonationsComponent },
     { path: '', component: WelcomeComponent },
     { path: '**', redirectTo: '' }
 ]
---
+++ src/app/views/sales-donations/sales-donations.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sales-donations',
+  standalone: true,
+  imports: [],
+  templateUrl: './sales-donations.component.html',
+  styleUrl: './sales-donations.component.scss'
+})
+export class SalesDonationsComponent {
+
+}
